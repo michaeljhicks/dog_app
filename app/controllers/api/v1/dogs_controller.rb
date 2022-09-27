0f99987da7951c814cecc3ec6fde9ef47d0a81fc
Michael Hicks <hicks.michael04@gmail.com>
2022-09-27 09:07:22 -0600

    create public and private actions in dogs controller

--- app/controllers/api/v1/dogs_controller.rb
+++ app/controllers/api/v1/dogs_controller.rb
@@ -1,2 +1,31 @@
 class Api::V1::DogsController < ApplicationController
+    before_action :find_dog, only: %i[update destroy]
+
+    def index
+        @dogs = Dog.order(name: :DESC)
+        render json: @dogs
+    end 
+
+    def create
+        @dog = Dog.new(dog_params)
+        @dog.save
+    end 
+
+    def destroy
+        @dog.delete
+    end 
+
+    def update
+        @dog.update(params.permit(:name, :breed))
+    end 
+
+    private
+
+    def dog_params 
+        params.permit(:name, :breed, :owner_id)
+    end 
+
+    def find_dog
+        @dog = Dog.find(params[:id])
+    end 
 end
