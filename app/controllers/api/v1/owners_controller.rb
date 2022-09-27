106d19bf64a81ebedc07f1ec15aca154ea332d34
Michael Hicks <hicks.michael04@gmail.com>
2022-09-27 09:19:41 -0600

    delete user model, add owner model

--- app/controllers/api/v1/owners_controller.rb
+++ app/controllers/api/v1/owners_controller.rb
@@ -1,2 +1,32 @@
 class Api::V1::OwnersController < ApplicationController
+    before_action :find_owner, only: %i[update destroy]
+
+    #notice: using .order instead of .all to order the JSON response based on the attribute params passed in
+    def index
+        @owners = Owner.order(name: :DESC)
+        render json: @owners
+    end
+
+    def create
+        @owner = Owner.save(owner_params)
+        @owner.save
+    end 
+
+    def destroy 
+        @owner.delete
+    end
+
+    def update
+        @owner.update(params.permit(:name))
+    end 
+
+    private 
+
+    def owner_params
+        params.permit(:name)
+    end 
+
+    def find_owner 
+        @owner = Owner.find(params[:id])
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_27_145618) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_27_151528) do
   create_table "dogs", force: :cascade do |t|
     t.string "name"
     t.string "breed"
@@ -18,6 +18,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_27_145618) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["owner_id"], name: "index_dogs_on_owner_id"
+  end
+
+  create_table "owners", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
