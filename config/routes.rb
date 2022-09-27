5efac559fd9d4dc20b9e62609e9428a0d2cb6237
Michael Hicks <hicks.michael04@gmail.com>
2022-09-27 09:02:05 -0600

    add owner dog resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do 
+    namespace :v1 do 
+      resources :owners, only: %i[index show update destroy]
+      resources :dogs, only: %i[index show update destroy]
+    end 
+  end 
 end
